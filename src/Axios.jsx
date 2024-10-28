0b4efec61557ee32c4cf3c395bae1972aa5891da
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-10-28 13:57:04 +0600

    add awesome component loader

--- src/Axios.jsx
+++ src/Axios.jsx
@@ -1,9 +1,13 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { BarChart, Bar, XAxis, YAxis } from "recharts";
+import { BarChart, Bar, XAxis } from "recharts";
+import { Audio, DNA } from "react-loader-spinner";
 
 export default function Axios() {
   const [phone, setPhone] = useState([]);
+
+  const [loading, setLoding] = useState(true);
+
   useEffect(() => {
     axios
       .get("https://openapi.programming-hero.com/api/phones?search=iphone")
@@ -18,11 +22,33 @@ export default function Axios() {
         });
         console.log(phonesWithFakeData);
         setPhone(phonesWithFakeData);
+        setLoding(false);
       });
   }, []);
   return (
-    <div>
-     
+    <div className="flex flex-col justify-center items-center">
+      <DNA
+        visible={true}
+        height="80"
+        width="80"
+        ariaLabel="dna-loading"
+        wrapperStyle={{}}
+        wrapperClass="dna-wrapper"
+      />
+      {loading && (
+        <div className="flex justify-center">
+          <Audio
+            height="80"
+            width="80"
+            radius="9"
+            color="green"
+            ariaLabel="loading"
+            wrapperStyle
+            wrapperClass
+          />
+          )
+        </div>
+      )}
       <BarChart width={550} height={140} data={phone}>
         <Bar dataKey="price" fill="#8884d8" />
         <XAxis dataKey="name"></XAxis>
